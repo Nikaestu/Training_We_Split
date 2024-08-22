b58ed32d1c8ffeb12e6d68c48c7f3e6b51287e1f
clone <lehir-dylan@hotmail.com>
2024-08-22 09:42:57 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dylan Le Hir on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
