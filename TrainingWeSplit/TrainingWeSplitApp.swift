ef1780d421522066e046f98f669e20987a603a67
clone <lehir-dylan@hotmail.com>
2024-11-04 10:52:57 +0100

    First commit

--- TrainingWeSplit/TrainingWeSplitApp.swift
+++ TrainingWeSplit/TrainingWeSplitApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct WeSplitApp: App {
+struct TrainingWeSplitApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplitView()
         }
     }
 }
